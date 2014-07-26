28cecee89f73662a29cc702fc612857e355cb599
x44x45x41x4E <d.canlas07@gmail.com>
2014-07-27 07:26:17 +0800

    Added seeds for RSVPStats

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,24 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 GameType.create([
-		{ :name => 'Card Game' }, 
-		{ :name => 'Board Game'}, 
-		{ :name => 'Video Game'}, 
+		{ :name => 'Card Game' },
+		{ :name => 'Board Game'},
+		{ :name => 'Video Game'},
 		{ :name => 'Sports'}
 	])
 
 Game.create([
-		{ :name => 'Magic the Gathering', :game_type_id => '1'}, 
-		{ :name => 'Card Fight Vanguard', :game_type_id => '1'}, 
-		{ :name => 'Monopoly Deal', :game_type_id => '1'}, 
-		{ :name => 'Pokemon TCG', :game_type_id => '1'}, 
-		{ :name => 'YU-GI-OH!', :game_type_id => '1'}, 
-		{ :name => 'Others', :game_type_id => '1'}, 
-		{ :name => 'Chess', :game_type_id => '2'}, 
-		{ :name => 'Hero Clix', :game_type_id => '2'}, 
-		{ :name => 'Battle Star Galactica', :game_type_id => '2'}, 
-		{ :name => 'Snake and Ladders', :game_type_id => '2'}, 
-		{ :name => 'Others', :game_type_id => '2'}, 
+		{ :name => 'Magic the Gathering', :game_type_id => '1'},
+		{ :name => 'Card Fight Vanguard', :game_type_id => '1'},
+		{ :name => 'Monopoly Deal', :game_type_id => '1'},
+		{ :name => 'Pokemon TCG', :game_type_id => '1'},
+		{ :name => 'YU-GI-OH!', :game_type_id => '1'},
+		{ :name => 'Others', :game_type_id => '1'},
+		{ :name => 'Chess', :game_type_id => '2'},
+		{ :name => 'Hero Clix', :game_type_id => '2'},
+		{ :name => 'Battle Star Galactica', :game_type_id => '2'},
+		{ :name => 'Snake and Ladders', :game_type_id => '2'},
+		{ :name => 'Others', :game_type_id => '2'},
 		{ :name => 'Dota2', :game_type_id => '3'},
 		{ :name => 'Dota', :game_type_id => '3'},
 		{ :name => 'LOL', :game_type_id => '3'},
@@ -35,28 +35,34 @@ Game.create([
 	])
 
 User.create([
-		{ 	:first_name => 'Bogart', 
+		{ 	:first_name => 'Bogart',
 			:last_name => 'Dope',
-			:email => 'sample@sample.com', 
+			:email => 'sample@sample.com',
 			:encrypted_password => '$2a$10$3e7SkGNW.dp0I./Lalz2NusFwhntY71w1/59Bg7RH3c4ipR1L.zLe',
-			:sign_in_count => '1', 
-			:current_sign_in_at => '2014-07-26 16:46:13', 
-			:last_sign_in_at => '2014-07-26 16:46:13', 
+			:sign_in_count => '1',
+			:current_sign_in_at => '2014-07-26 16:46:13',
+			:last_sign_in_at => '2014-07-26 16:46:13',
 			:current_sign_in_ip => '127.0.0.1',
-			:last_sign_in_ip => '127.0.0.1', 
-			:created_at => '2014-07-26 16:46:13', 
+			:last_sign_in_ip => '127.0.0.1',
+			:created_at => '2014-07-26 16:46:13',
 			:updated_at => '2014-07-26 16:46:13'
 			}
 	])
 
 TourneyType.create([
-		{ :tourney_name => 'Tournament' }, 
+		{ :tourney_name => 'Tournament' },
 		{ :tourney_name => 'Casual'}
 	])
 
 GameMode.create([
-		{ :name => 'Round Robin' }, 
-		{ :name => 'Scorer'}, 
+		{ :name => 'Round Robin' },
+		{ :name => 'Scorer'},
 		{ :name => 'Scorer'},
 		{ :name => 'Others'}
+	])
+
+RsvpStats.create([
+		{ :status => 'Join' }
+		{ :status => 'Maybe' }
+		{ :status => 'Not Going' }
 	])
