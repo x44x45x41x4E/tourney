43a416c80efc9cd646c43fe540fd8d7868718a22
Rommel David <rommel@gorated.com>
2014-07-27 08:03:46 +0800

    Redesigned user dashboard

--- app/models/tourney.rb
+++ app/models/tourney.rb
@@ -2,7 +2,7 @@ class Tourney < ActiveRecord::Base
   belongs_to :tourney_type
   belongs_to :tourney_comment
   belongs_to :tourney_comments
-  belongs_to :user
+  has_and_belongs_to_many :users
   belongs_to :venue
   belongs_to :game_type
 end
